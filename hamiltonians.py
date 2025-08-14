Fixed factor of 1/2 in sigma^+- definition of field operator

hamiltonians.py
@@ -1,5 +1,6 @@
 from collections.abc import Callable
 
+import numpy as np
 from qiskit.quantum_info import SparsePauliOp
 
 
@@ -82,7 +83,9 @@ class FreeWilson2D:
         j = self.j(m_x, n_y)
         prefix = j * "Z"
         postfix = (self.N_sites - j - 1) * "I"
-        return SparsePauliOp([prefix + "X" + postfix, adjoint * "-" + "i" + prefix + "Y" + postfix])
+        x_string = prefix + "X" + postfix
+        y_string = adjoint * "-" + "i" + prefix + "Y" + postfix
+        return SparsePauliOp([x_string, y_string], [0.5,0.5])
 
     def hamiltonian(self) -> SparsePauliOp:
         a = SparsePauliOp(self.N_sites * "I", 0)
@@ -101,3 +104,18 @@ class FreeWilson2D:
         b = b.simplify()
         c = c.simplify()
         return -0.5 * (c + c.adjoint() + self.r * (b + b.adjoint())) + (self.mass + 2 * self.r) * a
+
+    def zero_charge_penalty_term(self) -> SparsePauliOp:
+        ret = SparsePauliOp(self.N_sites * "I", 0)
+        for m_x in range(self.M_x):
+            for n_y in range(self.N_y):
+                ret += self.field(m_x, n_y, True) @ self.field(m_x, n_y, False)
+        ret -= SparsePauliOp(self.N_sites * "I", self.N_x*self.N_y)
+        return ret@ret
+
+    def size_of_zero_charge_sector(self) -> int:
+        pen_operator = self.zero_charge_penalty_term().simplify()
+        pen_matrix = pen_operator.to_matrix()
+        pen_list = pen_matrix.diagonal()
+        zeros = np.argwhere(np.isclose(pen_list, np.zeros_like(pen_list)))[:,0]
+        return len(zeros)

main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from qiskit.quantum_info import SparsePauliOp
 
 from hamiltonians import FreeWilson2D
 from scipy.sparse.linalg import eigsh
@@ -7,9 +8,9 @@ from scipy.sparse.linalg import eigsh
 def calc_energies(m: float, n_eigv = 10):
     print(f'Calculating energies for mass {m:2.3f}')
     free_wilson = FreeWilson2D(2, 2, m, 1)
-    h_operator = free_wilson.hamiltonian().simplify()
+    h_operator = (free_wilson.hamiltonian() + 100*free_wilson.zero_charge_penalty_term()).simplify()
     h_sparse_matrix = h_operator.to_matrix(sparse=True)
-    eigen_values, eigen_vectors = eigsh(h_sparse_matrix, k=n_eigv)
+    eigen_values, eigen_vectors = eigsh(h_sparse_matrix, k=n_eigv, which='SM')
     eigen_values : np.ndarray
     eigen_vectors : np.ndarray
     eigen_values.sort()
@@ -31,10 +32,10 @@ def plot_energies(ms: np.ndarray, sorted_energies: np.ndarray, n_plot: int, name
 
 
 masses = np.linspace(-6, 2, 501)
-energies = np.array([calc_energies(m, 10) for m in masses])
+zero_c_size = FreeWilson2D(2, 2, masses[0], 1).size_of_zero_charge_sector()
+print(f"Size of the zero charge sector: {zero_c_size}")
+energies = np.array([calc_energies(m, zero_c_size+2) for m in masses])
 print(masses.shape, energies.shape)
 plot_energy_gap(masses, energies)
 plot_energies(masses, energies, 2)
-
-
 