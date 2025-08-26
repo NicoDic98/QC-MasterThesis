13cd4be00214aa8f841825572223bf205776fe8a
s6nidich_hpc <ndichter@uni.bonn.de>
2025-08-26 21:23:57 +0200

    Basic circuit setup done

---
+++ solver/circuits.py
@@ -0,0 +1,58 @@
+from enum import StrEnum
+
+import h5py
+from qiskit import QuantumCircuit
+from qiskit.circuit import Parameter
+from qiskit.circuit.library import n_local, XXPlusYYGate
+
+
+class CircuitParameters(StrEnum):
+    Ansatz = "Ansatz"
+    NumQubits = "NumQubits"
+    NumLayers = "NumLayers"
+    NumParameters = "NumParameters"
+
+
+class BaseAnsatz:
+    def __init__(self, num_qubits: int, num_layers: int):
+        self.num_qubits = num_qubits
+        if num_layers < 1:
+            raise ValueError("Number of layers must be positive")
+        self.num_layers = num_layers
+        self.full_ansatz = QuantumCircuit(self.num_qubits)
+
+    def __call__(self):
+        return self.full_ansatz.copy()
+
+    def save_parameters(self, group: h5py.Group):
+        group.attrs[CircuitParameters.Ansatz] = type(self).__name__
+        group.attrs[CircuitParameters.NumQubits] = self.num_qubits
+        group.attrs[CircuitParameters.NumLayers] = self.num_layers
+        group.attrs[CircuitParameters.NumParameters] = self.full_ansatz.num_parameters
+
+
+
+class XXPlusYYRZAnsatz1(BaseAnsatz):
+    def __init__(self, num_qubits: int, num_layers: int):
+        super().__init__(num_qubits, num_layers)
+        initial_block = n_local(num_qubits=self.num_qubits,
+                                rotation_blocks=[],
+                                entanglement_blocks=XXPlusYYGate(Parameter('A'), 0),
+                                entanglement="pairwise",
+                                insert_barriers=True,
+                                skip_final_rotation_layer=True,
+                                reps=1,
+                                parameter_prefix="phi")
+        following_blocks = n_local(num_qubits=self.num_qubits,
+                                   rotation_blocks="rz",
+                                   entanglement_blocks=XXPlusYYGate(Parameter('A'), 0),
+                                   entanglement="pairwise",
+                                   insert_barriers=True,
+                                   reps=self.num_layers - 1)  # this adds just rotations if layers - 1 = 0
+
+        self.variational_ansatz = initial_block.compose(following_blocks)
+        # todo: add initialization for zero charge sector
+        self.full_ansatz = self.variational_ansatz
+
+    def save_parameters(self, group: h5py.Group):
+        super().save_parameters(group)
--- solver/variational_quantum_eigensolver.py
+++ solver/variational_quantum_eigensolver.py
@@ -1,45 +1,46 @@
 from typing import Callable
 
 import h5py
-import matplotlib.pyplot as plt
-from qiskit.circuit import Parameter
-from qiskit.circuit.library import n_local, XXPlusYYGate
+from qiskit.transpiler import generate_preset_pass_manager
+from qiskit_aer import AerSimulator
 
-from hamiltonian.free_wilson import BaseHamiltonian
 from hamiltonian.base import HamiltonianType
+from hamiltonian.free_wilson import BaseHamiltonian
 from solver.base import BaseSolver
+from solver.circuits import XXPlusYYRZAnsatz1
 
 
 class VQE(BaseSolver):
     def __init__(self,
                  hamiltonian_factory: Callable[..., BaseHamiltonian],
                  save_group: h5py.Group,
-                 layers: int,
-                 num_qubits: int, ):
+                 num_qubits: int,
+                 num_layers: int, ):
         super().__init__(hamiltonian_factory, save_group)
-        if layers < 1:
-            raise ValueError("Number of layers must be positive")
-        initial_block = n_local(num_qubits=num_qubits,
-                                rotation_blocks=[],
-                                entanglement_blocks=XXPlusYYGate(Parameter('A'), 0),
-                                entanglement="pairwise",
-                                insert_barriers=True,
-                                skip_final_rotation_layer=True,
-                                reps=1,
-                                parameter_prefix="phi")
-        following_blocks = n_local(num_qubits=num_qubits,
-                                   rotation_blocks="rz",
-                                   entanglement_blocks=XXPlusYYGate(Parameter('A'), 0),
-                                   entanglement="pairwise",
-                                   insert_barriers=True,
-                                   reps=layers - 1)  # this adds just rotations if layers - 1 = 0
-        self.ansatz = initial_block.compose(following_blocks)
-        self.ansatz.draw("mpl")
-        plt.show()
+        self.ansatz = XXPlusYYRZAnsatz1(num_qubits, num_layers)
 
     def run(self, parameters_dict_list: dict[str, list], hamiltonian_type: HamiltonianType, n_steps: int = 100):
         local_group, h5_saver, test_hamiltonian = self.initialize_run(parameters_dict_list, hamiltonian_type)
+        self.ansatz.save_parameters(local_group)
         test_hamiltonian_op = test_hamiltonian.hamiltonian_op(hamiltonian_type)
         if test_hamiltonian_op.num_qubits != self.ansatz.num_qubits:
             raise ValueError(
                 f"Number of qubits does not match ansatz: {test_hamiltonian_op.num_qubits}!={self.ansatz.num_qubits}")
+
+        qc = self.ansatz()
+
+        # See:
+        # https://qiskit.github.io/qiskit-aer/tutorials/1_aersimulator.html
+        backend = AerSimulator()
+
+        # See for more pass manager options:
+        # https://quantum.cloud.ibm.com/docs/en/guides/defaults-and-configuration-options
+        pm = generate_preset_pass_manager(backend=backend,
+                                          optimization_level=3,
+                                          approximation_degree=1.00,
+                                          seed_transpiler=42)
+
+        # this will be the same as qc, if backend = AerSimulator()
+        isa_circuit = pm.run(qc)
+
+        hamiltonian_isa = test_hamiltonian_op.apply_layout(layout=isa_circuit.layout)
