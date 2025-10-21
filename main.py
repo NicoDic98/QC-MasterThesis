923e297048f88cbab6bf6cbed8c5d6e54dc43b81
s6nidich_hpc <ndichter@uni.bonn.de>
2025-10-21 18:52:23 +0200

    Ran adapt vqe with the full pool when using the hamiltonian without penalty term for the derivative calculation in Adapt VQE for 10 masses look better but far from good

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def run_adapt_vqe(my_f: h5py.File):
     my_adapt_vqe = AdaptVQE(FreeWilson2D.build_hamiltonian, my_f, 8, 40)
     my_adapt_vqe.run({HamiltonianParameters.XExtend: [2],
                       HamiltonianParameters.YExtend: [2],
-                      HamiltonianParameters.Mass: np.linspace(-6, 2, 2).tolist(),
+                      HamiltonianParameters.Mass: np.linspace(-6, 2, 10).tolist(),
                       HamiltonianParameters.WilsonParameter: [1.]},
                      HamiltonianType.ZeroChargePenalty,
                      # simulator_type=SimulatorType.Aer,
