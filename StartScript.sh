ad5131d3e89ce8e7ae1c885dc2c9f51867085555
NicoMainPC <nd@nocoffeetech.de>
2025-11-06 10:49:37 +0100

    Some local tests, seems like th OOM should have happened immediately

--- StartScript.sh
+++ StartScript.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=intelsr_short
 #SBATCH --time=0-07:59:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --mem=5G
 #SBATCH --job-name=MyPyJob
 #SBATCH --output=out/run.%A.out
 #SBATCH --account=ag_hiskp_funcke
