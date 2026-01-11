33b634fbc95f408fabe7a8c035784ed87e33470c
s6nidich_hpc <ndichter@uni.bonn.de>
2026-01-11 12:21:18 +0100

    Measured C for the prev. best results

--- main.py
+++ main.py
@@ -244,20 +244,23 @@ Path(data_folder).mkdir(parents=True, exist_ok=True)
 
 h5_file = f"{data_folder}{datetime.now().strftime('%Y-%m-%U')}-{args.id}"
 
-with h5py.File(h5_file + ".hdf5", "w", libver='latest') as f:
-    pprint_h5(f)
-    f.attrs[GlobalParameters.ProcessId] = args.id
-    # run_ed(f)
-    # run_vqe(f)
-    run_adapt_vqe(f)
-    # with h5py.File(f"{data_folder}{"2025-11-46"}.hdf5", "r") as sf:
-    #     rerun_vqe(f, sf["2025-11-20_17-57-51-23964135"])
-    pprint_h5(f)
+# with h5py.File(h5_file + ".hdf5", "w", libver='latest') as f:
+#     pprint_h5(f)
+#     f.attrs[GlobalParameters.ProcessId] = args.id
+#     # run_ed(f)
+#     # run_vqe(f)
+#     run_adapt_vqe(f)
+#     # with h5py.File(f"{data_folder}{"2025-11-46"}.hdf5", "r") as sf:
+#     #     rerun_vqe(f, sf["2025-11-20_17-57-51-23964135"])
+#     pprint_h5(f)
 
-# with h5py.File(f"{data_folder}{"2026-01-01"}.hdf5", "a") as sf:
-#     pprint_h5(sf)
-#     measure_observables(sf["2026-01-07_14-31-34-24236398"])
-#     pprint_h5(sf)
+file_names = ["2025-11-45", "2026-01-02", "2026-01-02"]
+dataset_names = ["2025-11-13_14-45-01-23862036", "2026-01-11_00-57-15-24245100", "2026-01-11_01-16-50-24245112"]
+for file_name, dataset_name in zip(file_names, dataset_names):
+    with h5py.File(f"{data_folder}{file_name}.hdf5", "a") as sf:
+        pprint_h5(sf)
+        measure_observables(sf[dataset_name])
+        pprint_h5(sf)
 
 # with h5py.File(f"{data_folder}{"2025-09-35"}.hdf5", "a") as sf:
 #     pprint_h5(sf)
